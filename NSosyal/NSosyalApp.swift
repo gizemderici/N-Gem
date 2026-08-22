6553b670b6a366d071260d0b7a7253cf2a40f9c3
Furkan Durmaz <furkandurmaz5050@gmail.com>
2026-08-22 20:18:02 +0300

    Initial Commit

---
+++ NSosyal/NSosyalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSosyalApp.swift
+//  NSosyal
+//
+//  Created by Furkan Durmaz on 22.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct NSosyalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
